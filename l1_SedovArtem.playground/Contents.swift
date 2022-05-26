124784d82360b47844a423fd917c4266a3f11a1a
metrovodes <70141099+metrovodes@users.noreply.github.com>
2022-05-27 02:45:19 +0300

    Задание 1

--- l1_SedovArtem.playground/Contents.swift
+++ l1_SedovArtem.playground/Contents.swift
@@ -1,3 +1,30 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+// Задание № 1. Решение квадратного уравнения
+
+let a: Double = 2
+let b: Double = 100
+let c: Double = 45
+
+let D: Double = b * b - 4 * a * c
+
+
+if D < 0 {
+    
+    print("Решений уравнения нет")
+    
+}else {
+    
+    if D == 0{
+        
+        let x = (b * (-1) - D) / (2 * a) + 0
+        print("x = \(x)")
+        
+    }else {
+        
+        let x1 = (b * (-1) - sqrt(D) ) / (2 * a)
+        let x2 = (b * (-1) + sqrt(D) ) / (2 * a)
+        print("x1 = \(x1)\nx2 = \(x2)")
+        
+    }
+}
