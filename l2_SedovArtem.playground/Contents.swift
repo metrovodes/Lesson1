a9f1a3e9181489353d3822078d602acb1938e7bc
metrovodes <70141099+metrovodes@users.noreply.github.com>
2022-05-27 16:40:20 +0300

    Задание 1 и 2

---
+++ l2_SedovArtem.playground/Contents.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+// Задание № 1. Проверяем, является ли число четным
+
+func isEven(number n: Int) -> Bool {
+    let res: Bool
+    if n % 2 == 0 {
+        res = true
+    }else {
+        res = false
+    }
+    return res
+}
+
+// Задание № 2. Проверяем, делится ли число на 3 без остатка
+func divideThree(number n : Int) -> Bool {
+    let res: Bool
+    if n % 3 == 0 {
+        res = true
+    }else {
+        res = false
+    }
+    return res
+}
+
