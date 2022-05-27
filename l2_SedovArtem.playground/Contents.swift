1461103595232f6a5b20940784421a6dd616f8d6
metrovodes <70141099+metrovodes@users.noreply.github.com>
2022-05-27 17:13:40 +0300

    Задание 4

--- l2_SedovArtem.playground/Contents.swift
+++ l2_SedovArtem.playground/Contents.swift
@@ -27,3 +27,15 @@ func divideThree(number n : Int) -> Bool {
 
 var numbers: [Int] = Array(1...100)
 
+// Задание № 4. Отфильтровать массив с помощью функций из заданий 1 и 2.
+
+func checkElements (arrayToCheck a: [Int]) -> Array<Int> {
+    var newNumbers: [Int] = []
+    for i in a {
+        if isEven(number: i) && !divideThree(number: i) {
+            newNumbers.append(i)
+        }
+    }
+    return newNumbers
+}
+
