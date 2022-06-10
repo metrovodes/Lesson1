03eaf9d18e2f58cad55e1176ad91287d431dc89e
metrovodes <70141099+metrovodes@users.noreply.github.com>
2022-06-10 11:34:18 +0300

    Задание 1

--- l5_SedovArtem.playground/Contents.swift
+++ l5_SedovArtem.playground/Contents.swift
@@ -1,3 +1,17 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+enum engineStatus{
+    case on
+    case off
+}
+
+enum windowsStatus{
+    case opened
+    case closed
+}
+protocol Car{
+    var brand: String { get }
+    var year: Int { get }
+    var windows: windowsStatus { get set }
+    var engine: engineStatus { get set }
+}
