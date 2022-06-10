87ab972c61daab2941f1c26308530c5caefdb76b
metrovodes <70141099+metrovodes@users.noreply.github.com>
2022-06-10 18:46:26 +0300

    Update Contents.swift

--- l5_SedovArtem.playground/Contents.swift
+++ l5_SedovArtem.playground/Contents.swift
@@ -68,10 +68,53 @@ extension TrunkCar{
         switch self.currentLoad + w{
         case self.trailerMaxLoad...:
             self.currentLoad = self.trailerMaxLoad
-        case 0...self.currentLoad:
-            self.currentLoad += w
-        default:
+        case ...self.currentLoad:
             self.currentLoad = 0
+        default:
+            self.currentLoad += w
         }
     }
 }
+
+class trunkCar: TrunkCar{
+    var brand: String
+    var year: Int
+    var windows: windowsStatus = .closed
+    var engine: engineStatus = .off
+    var trailerMaxLoad: Int
+    var currentLoad: Int = 0
+    
+    init(name: String, releaseYear: Int, trailerMLoad: Int){
+        self.brand = name
+        self.year = releaseYear
+        self.trailerMaxLoad = trailerMLoad
+    }
+}
+
+extension trunkCar: CustomStringConvertible{
+    var description: String{
+        let es: String
+        switch self.engine {
+        case .off:
+            es = "Двигатель выключен."
+        case .on:
+            es = "Двигатель запущен."
+        }
+        let ws: String
+        switch self.windows{
+        case .closed:
+            ws = "Окна закрыты."
+        case .opened:
+            ws = "Окна открыты."
+        }
+        return "Грузовик \(self.brand). \(self.year) года выпуска.\n\(es) \(ws)\nТекущая загрузка кузова: \(self.currentLoad) из \(self.trailerMaxLoad) кг."
+    }
+}
+
+var man = trunkCar(name: "Man", releaseYear: 2006, trailerMLoad: 500)
+
+man.loadTrailer(weight: 222)
+man.windowsOpenClose(.opened)
+man.engineOnOff(.on)
+
+print(man)
