2d2887322fa00b8697c3244c83320ad643ea3440
metrovodes <70141099+metrovodes@users.noreply.github.com>
2022-06-10 17:56:33 +0300

    Задание 3

--- l5_SedovArtem.playground/Contents.swift
+++ l5_SedovArtem.playground/Contents.swift
@@ -36,3 +36,42 @@ extension Car {
         }
     }
 }
+
+protocol SportCar: Car{
+    var topSpeed: Int { get }
+    var currentSpeed: Int { get set }
+}
+
+extension SportCar{
+    mutating func accelerate (speed s: Int){
+        switch self.currentSpeed + s{
+        case topSpeed...:
+            self.currentSpeed = self.topSpeed
+            print("Достигнута максимальная скорость")
+        case 0...topSpeed:
+            self.currentSpeed += s
+            print("Текущая скорость - \(self.currentSpeed) км/ч")
+        default:
+            self.currentSpeed = 0
+            print("Остановились")
+        }
+    }
+}
+
+protocol TrunkCar: Car{
+    var trailerMaxLoad: Int { get set }
+    var currentLoad: Int { get set }
+}
+
+extension TrunkCar{
+    mutating func loadTrailer (weight w: Int){
+        switch self.currentLoad + w{
+        case self.trailerMaxLoad...:
+            self.currentLoad = self.trailerMaxLoad
+        case 0...self.currentLoad:
+            self.currentLoad += w
+        default:
+            self.currentLoad = 0
+        }
+    }
+}
