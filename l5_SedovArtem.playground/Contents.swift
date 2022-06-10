588f218beeef10ba4daad7d222ec945b2cb2fae7
metrovodes <70141099+metrovodes@users.noreply.github.com>
2022-06-10 11:40:43 +0300

    Update Contents.swift

--- l5_SedovArtem.playground/Contents.swift
+++ l5_SedovArtem.playground/Contents.swift
@@ -9,9 +9,30 @@ enum windowsStatus{
     case opened
     case closed
 }
+
 protocol Car{
     var brand: String { get }
     var year: Int { get }
     var windows: windowsStatus { get set }
     var engine: engineStatus { get set }
+}
+
+extension Car {
+    mutating func windowsOpenClose (_ w: windowsStatus){
+        switch w{
+        case .closed:
+            self.windows = .closed
+        case .opened:
+            self.windows = .opened
+        }
+    }
+    
+    mutating func engineOnOff (_ e: engineStatus){
+        switch e{
+        case .off:
+            self.engine = .off
+        case .on:
+            self.engine = .on
+        }
+    }
 }
