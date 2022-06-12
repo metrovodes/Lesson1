c628fc4ca704d382260b2b4cabe546090289643a
metrovodes <70141099+metrovodes@users.noreply.github.com>
2022-06-12 22:55:27 +0300

    Метод класса для открытия дверцы и перечисление со статусом машинки(занята или свободна)

--- l7_SedovArtem.playground/Contents.swift
+++ l7_SedovArtem.playground/Contents.swift
@@ -7,30 +7,47 @@ enum door{
 
 enum mode{
     case wash
-    case free
     case rinsing
     case spin
     case off
 }
 
-enum stiralkaErrors: Error{
+enum washerErrors: Error{
     case noPower
     case doorClosed
     case doorOpened
     case maxLoad
     case wrongMode
+    case busy
 }
 
-class Stiralka{
+enum washerStatus{
+    case busy
+    case free
+}
+
+class Washer{
     let brand: String
     let maxLoad: Int
     var currentLoad: Int = 0
     var doorStatus: door = .closed
     var mode: mode = .off
+    var status: washerStatus = .free
     
     init(brand: String, maxLoad: Int){
         self.brand = brand
         self.maxLoad = maxLoad
     }
     
+    func doorOpenClose(_ a: door) throws{
+        guard self.mode == .off else {
+            throw washerErrors.noPower
+        }
+        
+        guard self.status == .free{
+            throw washerErrors.busy
+        }
+        
+        self.doorStatus = a
+    }
 }
