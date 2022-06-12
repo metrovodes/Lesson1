5fdaf345365b168da998d8362acd3eb46a878886
metrovodes <70141099+metrovodes@users.noreply.github.com>
2022-06-12 22:45:40 +0300

    Создание класса и перечислений для работы

--- l7_SedovArtem.playground/Contents.swift
+++ l7_SedovArtem.playground/Contents.swift
@@ -1,1 +1,36 @@
 import Foundation
+
+enum door{
+    case opened
+    case closed
+}
+
+enum mode{
+    case wash
+    case free
+    case rinsing
+    case spin
+    case off
+}
+
+enum stiralkaErrors: Error{
+    case noPower
+    case doorClosed
+    case doorOpened
+    case maxLoad
+    case wrongMode
+}
+
+class Stiralka{
+    let brand: String
+    let maxLoad: Int
+    var currentLoad: Int = 0
+    var doorStatus: door = .closed
+    var mode: mode = .off
+    
+    init(brand: String, maxLoad: Int){
+        self.brand = brand
+        self.maxLoad = maxLoad
+    }
+    
+}
